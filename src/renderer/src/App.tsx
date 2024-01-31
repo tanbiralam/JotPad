6eec3cce6aa82ca732cf784ffec81624e92b39cb
Tanbir Alam <itzmetanbir@gmail.com>
2024-01-31 13:41:18 +0530

    sidebar fixed

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,11 @@
-import { ActionButtonRow, Content, DraggableTopBar, RootLayout, Sidebar } from './components'
+import {
+  ActionButtonRow,
+  Content,
+  DraggableTopBar,
+  NotePreviewList,
+  RootLayout,
+  Sidebar
+} from './components'
 
 const App = () => {
   return (
@@ -7,6 +14,7 @@ const App = () => {
       <RootLayout>
         <Sidebar className="p-2">
           <ActionButtonRow className="flex justify-between mt-1" />
+          <NotePreviewList className="mt-3 space-y-1" />
         </Sidebar>
         <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
       </RootLayout>
@@ -15,5 +23,3 @@ const App = () => {
 }
 
 export default App
-
-//https://youtu.be/t8ane4BDyC8?t=3334
