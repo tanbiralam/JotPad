8187802e8e6fd4f3e6a4ad4554bc9546e5eb5b4e
Tanbir Alam <itzmetanbir@gmail.com>
2024-01-31 22:30:15 +0530

    markdown editor implemented

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,6 +2,8 @@ import {
   ActionButtonRow,
   Content,
   DraggableTopBar,
+  FloatingNoteTitle,
+  MDEditor,
   NotePreviewList,
   RootLayout,
   Sidebar
@@ -16,7 +18,10 @@ const App = () => {
           <ActionButtonRow className="flex justify-between mt-1" />
           <NotePreviewList className="mt-3 space-y-1" />
         </Sidebar>
-        <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
+        <Content className="border-l bg-zinc-900/50 border-l-white/20">
+          <FloatingNoteTitle className="pt-2" />
+          <MDEditor />
+        </Content>
       </RootLayout>
     </>
   )
