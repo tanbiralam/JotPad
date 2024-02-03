63a485d3849a27565d482a0605814238d282a96e
tanbiralam <itzmetanbir@gmail.com>
2024-02-03 23:27:53 +0530

    note edit and delete function done

--- src/renderer/src/components/Button/AddNote.tsx
+++ src/renderer/src/components/Button/AddNote.tsx
@@ -1,9 +1,17 @@
-import { ActionButton, ActionButtonProps } from './ActionButton'
+import { createNote } from '@renderer/store'
+import { useSetAtom } from 'jotai'
 import { LuFileSignature } from 'react-icons/lu'
+import { ActionButton, ActionButtonProps } from './ActionButton'
 
 export const AddNote = ({ ...props }: ActionButtonProps) => {
+  const createEmptyNote = useSetAtom(createNote)
+
+  const handleCreate = () => {
+    createEmptyNote()
+  }
+
   return (
-    <ActionButton {...props}>
+    <ActionButton onClick={handleCreate} {...props}>
       <LuFileSignature className="w-4 h-4 text-zinc-600" />
     </ActionButton>
   )
--- src/renderer/src/components/Button/DeleteNote.tsx
+++ src/renderer/src/components/Button/DeleteNote.tsx
@@ -1,9 +1,16 @@
+import { deleteNote } from '@renderer/store'
+import { useSetAtom } from 'jotai'
 import { FaRegTrashCan } from 'react-icons/fa6'
 import { ActionButton, ActionButtonProps } from './ActionButton'
 
 export const DeleteNote = ({ ...props }: ActionButtonProps) => {
+  const deleteEmptyNote = useSetAtom(deleteNote)
+  const handleDelete = () => {
+    deleteEmptyNote()
+  }
+
   return (
-    <ActionButton {...props}>
+    <ActionButton onClick={handleDelete} {...props}>
       <FaRegTrashCan className="w-4 h-4 text-zinc-600" />
     </ActionButton>
   )
