a510e7c330046c97096c5bf745a83518c8b14a98
tanbiralam <itzmetanbir@gmail.com>
2024-03-15 03:47:05 +0530

    some minor error fixed

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,4 +1,4 @@
 export const appDirName = 'JotPad'
 export const fileEncoding = 'utf-8'
 export const autoSaveTime = 3000
-export const welcomeNoteFileName = 'Welcome.md'
+export const welcomeNoteFileName = 'welcomeNote.md'
