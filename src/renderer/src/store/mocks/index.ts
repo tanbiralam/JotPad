d0c7433a6e7a9612c12fc6fa9aac9cccc90d67f2
tanbiralam <itzmetanbir@gmail.com>
2026-02-19 16:40:02 +0530

    fix: update package.json with correct app name and author, enhance notesMock with missing ext property

--- src/renderer/src/store/mocks/index.ts
+++ src/renderer/src/store/mocks/index.ts
@@ -3,18 +3,22 @@ import { NoteInfo } from '@shared/models'
 export const notesMock: NoteInfo[] = [
   {
     title: 'Welcome 👋',
-    lastEditTime: new Date().getTime()
+    lastEditTime: new Date().getTime(),
+    ext: ''
   },
   {
     title: 'Hello From Note 1',
-    lastEditTime: new Date().getTime()
+    lastEditTime: new Date().getTime(),
+    ext: ''
   },
   {
     title: 'Hello From Note 2',
-    lastEditTime: new Date().getTime()
+    lastEditTime: new Date().getTime(),
+    ext: ''
   },
   {
     title: 'Hello From Note 3',
-    lastEditTime: new Date().getTime()
+    lastEditTime: new Date().getTime(),
+    ext: ''
   }
 ]
